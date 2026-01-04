a07c290b488045e032187ae475240fa5a484aefa
Sputchik <sputchik@gmail.com>
2026-01-04 19:47:05 +0100

    bat

--- build.bat
+++ build.bat
@@ -1,8 +1,11 @@
 @echo off
 call devcmd
-if exist build ( rmdir /s /q build )
-mkdir build
-cd build
 
-cmake -G Ninja -S ../src/ -B . -DCMAKE_C_FLAGS="/Qpar /GL /arch:AVX2 /O2 /Ob2 /Gw /Gy /fp:fast" -DCMAKE_CXX_FLAGS="/arch:AVX2 /O2 /Ob2 /Gw /Gy /fp:fast" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:/dev/deps/" -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON -DZLIB_LIBRARY="C:/dev/deps/lib/zlib-ng.lib"
+set "BUILD_DIR=build"
+if exist "%BUILD_DIR%" ( rmdir /s /q "%BUILD_DIR%" )
+mkdir "%BUILD_DIR%"
+cd "%BUILD_DIR%"
+
+cmake -G Ninja -S ../src/ -B . -DCMAKE_C_FLAGS="/Qpar /GL /O2 /Ob2 /Gw /Gy /fp:fast" -DCMAKE_CXX_FLAGS="/Qpar /GL /O2 /Ob2 /Gw /Gy /fp:fast" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:/dev/deps/" -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON
 ninja -j4
+cd ..
